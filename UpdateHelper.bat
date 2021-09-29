b1cdab9dff5fadbbe78ef136aca4473b623314e1
Maxi <maxi.krause@me.com>
2021-09-29 10:08:31 +0200

    Add files via upload

--- UpdateHelper.bat
+++ UpdateHelper.bat
@@ -3,5 +3,6 @@ title Updater
 echo Will now update EXE to intunewin
 del IntuneSetupFile.bat
 ren IntuneSetupFile-Latest.bat IntuneSetupFile.bat
+start IntuneSetupFile.bat
 goto 2>nul & del "%~f0"
 exit
